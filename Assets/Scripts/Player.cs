7f5450deee5e828eaf5f1437210c764e2bbb4f29
Ertan Turan <ertanturan@outlook.com>
2019-09-22 14:48:14 +0300

    player movement done .

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float MoveSpeed = 600f;
+
+    private float _movement = 0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero,Vector3.forward , 
+            _movement * Time.fixedDeltaTime * -MoveSpeed );
     }
 }
