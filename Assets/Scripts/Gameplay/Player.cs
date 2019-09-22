8f814e4e50df23ee57c6770bdd78576262f14d82
Ertan Turan <ertanturan@outlook.com>
2019-09-22 15:51:40 +0300

    window kit removed .

--- Assets/Scripts/Gameplay/Player.cs
+++ Assets/Scripts/Gameplay/Player.cs
@@ -8,8 +8,6 @@ public class Player : MonoBehaviour
 
     private float _movement = 0f;
 
-    [Header("UI")] [SerializeField] private WindowUI WindowFail;
-
     private void Update()
     {
         _movement = Input.GetAxisRaw("Horizontal");
@@ -17,14 +15,21 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.RotateAround(Vector3.zero,Vector3.forward , 
-            _movement * Time.fixedDeltaTime * -MoveSpeed );
+        transform.RotateAround(Vector3.zero, Vector3.forward,
+            _movement * Time.fixedDeltaTime * -MoveSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("PLAYER FAILED..");
         Spawner.Instance.IsSpawnable = false;
-        WindowFail.Show();
+        StartCoroutine(DelayedPause());
+        Time.timeScale = 0;
+    }
+
+    private IEnumerator DelayedPause()
+    {
+        yield return new WaitForSecondsRealtime(.25f);
+    
     }
 }
